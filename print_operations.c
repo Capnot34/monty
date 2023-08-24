cd6820fd9ef4e37be8b2af4f969a37b0a3ece335
Gift Amachree <128798186+Capnot34@users.noreply.github.com>
2023-08-24 19:42:12 +0100

    Update print_operations.c

--- print_operations.c
+++ print_operations.c
@@ -1,12 +1,12 @@
 #include "monty.h"
 
 /**
- * pint_op - prints the value at the top of the stack.
+ * pint - prints the value at the top of the stack.
  * @stack: double pointer to the top of the stack.
  * @line_number: line number where the instruction appears.
  * Return: nothing.
  */
-void pint_op(stack_t **stack, unsigned int line_number)
+void pint(stack_t **stack, unsigned int line_number)
 {
     if (!stack || !*stack)
     {
