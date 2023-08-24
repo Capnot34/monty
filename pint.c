26a4f812bde96fc9db24b277bd14d88ab369659e
Gift Amachree <128798186+Capnot34@users.noreply.github.com>
2023-08-24 14:00:06 +0100

    Update pint.c

--- pint.c
+++ pint.c
@@ -1,15 +1,15 @@
 #include "monty.h"
 /**
- * f_pint - prints the top
- * @head: stack head
- * @counter: line_number
+ * pint - prints the top
+ * @stack: stack head
+ * @line_number: line_number
  * Return: no return
 */
-void f_pint(stack_t **head, unsigned int counter)
+void pint(stack_t **stack, unsigned int line_number)
 {
 	if (*head == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
