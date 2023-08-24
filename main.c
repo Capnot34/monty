7edf7d9fd1eb442007c683d5322822c10387ed97
Gift Amachree <128798186+Capnot34@users.noreply.github.com>
2023-08-24 17:21:59 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,12 @@
 #include "monty.h"
 
-/**
- * main - Monty bytecode interpreter
- * @argc: argument count
- * @argv: argument values
- * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
- */
 int main(int argc, char **argv)
 {
     FILE *file;
-    char *line = NULL;
+    char *line = NULL, *opcode, *argument;
     size_t len = 0;
+    unsigned int line_number = 0;
+    stack_t *stack = NULL; 
 
     if (argc != 2)
     {
@@ -27,12 +23,44 @@ int main(int argc, char **argv)
 
     while (getline(&line, &len, file) != -1)
     {
-        /* Parse line to get opcode and arguments */
-        /* Execute opcode function */
+        line_number++;
+
+        opcode = strtok(line, " \n\t");
+        argument = strtok(NULL, " \n\t");
+
+        // If opcode is "push"
+        if (strcmp(opcode, "push") == 0)
+        {
+            if (!argument || !isdigit(argument[0]))
+            {
+                fprintf(stderr, "L%d: usage: push integer\n", line_number);
+                free(line);
+                free_stack(&stack);
+                exit(EXIT_FAILURE);
+            }
+            push(&stack, atoi(argument));
+        }
+        // If opcode is "pall"
+        else if (strcmp(opcode, "pall") == 0)
+            pall(&stack, line_number);
+        // If opcode is "pint"
+        else if (strcmp(opcode, "pint") == 0)
+            pint(&stack, line_number);
+        // ... Add more opcode comparisons here ...
+
+        // If opcode is not recognized
+        else
+        {
+            fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+            free(line);
+            free_stack(&stack);
+            exit(EXIT_FAILURE);
+        }
     }
 
     free(line);
+    free_stack(&stack); 
     fclose(file);
+
     return EXIT_SUCCESS;
 }
-
