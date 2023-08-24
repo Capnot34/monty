e3241921aeba119a8d0d98945e48ac1cab8450d4
Gift Amachree <128798186+Capnot34@users.noreply.github.com>
2023-08-24 21:08:38 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -56,5 +56,6 @@ void rotr_op(stack_t **stack, unsigned int line_number);
 void stack_op(stack_t **stack, unsigned int line_number);
 void queue_op(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t **stack);
+int is_valid_int(const char *str);
 
 #endif /* MONTY_H */
