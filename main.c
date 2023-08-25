65d777c37e00f2a0d641edb0baad66ca4d9b7f90
Gift Amachree <128798186+Capnot34@users.noreply.github.com>
2023-08-25 11:08:21 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,109 +1,126 @@
 #include "monty.h"
 
-/**
- * handle_push - Handles the 'push' opcode
- * @stack: Double pointer to the top of the stack
- * @argument: The argument passed with the opcode
- * @line_number: Current line number
- */
-void handle_push(stack_t **stack, char *argument, unsigned int line_number)
+void process_opcode(stack_t **stack, char *opcode, char *argument,
+unsigned int line_number)
 {
-	if (!argument || !is_valid_int(argument))
+	/* If opcode is "push" */
+	if (strcmp(opcode, "push") == 0)
 	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		if (!argument || !is_valid_int(argument))
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			free_stack(stack);
+			exit(EXIT_FAILURE);
+		}
+		push(stack, atoi(argument), line_number);
+	}
+	/* If opcode is "pall" */
+	else if (strcmp(opcode, "pall") == 0)
+	{
+		pall(stack, line_number);
+	}
+	/* If opcode is "pint" */
+	else if (strcmp(opcode, "pint") == 0)
+	{
+		pint(stack, line_number);
+	}
+	/* If opcode is "nop" */
+	else if (strcmp(opcode, "nop") == 0)
+	{
+		/* Do nothing, continue with the next opcode */
+	}
+	/* If opcode is "pchar" */
+	else if (strcmp(opcode, "pchar") == 0)
+	{
+		pchar_op(stack, line_number);
+	}
+	/* If opcode is "swap" */
+	else if (strcmp(opcode, "swap") == 0)
+	{
+		swap(stack, line_number);
+	}
+	/* If opcode is "pop" */
+	else if (strcmp(opcode, "pop") == 0)
+	{
+	pop(stack, line_number);
+	}
+	/* If opcode is "add" */
+	else if (strcmp(opcode, "add") == 0)
+	{
+		add(stack, line_number);
+	}
+	/* If opcode is "sub" */
+	else if (strcmp(opcode, "sub") == 0)
+	{
+		sub(stack, line_number);
+	}
+	/* If opcode is "mul" */
+	else if (strcmp(opcode, "mul") == 0)
+	{
+		mul_op(stack, line_number);
+	}
+	/* If opcode is "div" */
+	else if (strcmp(opcode, "div") == 0)
+	{
+		div_op(stack, line_number);
+	}
+	/* If opcode is "mod" */
+	else if (strcmp(opcode, "mod") == 0)
+	{
+		mod_op(stack, line_number);
+	}	
+	/* ... Add more opcode comparisons here ... */
+	else
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
 		free_stack(stack);
 		exit(EXIT_FAILURE);
 	}
-	push(stack, atoi(argument), line_number);
-}
-void handle_pall(stack_t **stack, char *argument, unsigned int line_number)
-{
-	(void) argument;
-	pall(stack, line_number);
-}
-void handle_pint(stack_t **stack, char *argument, unsigned int line_number)
-{
-	(void) argument;
-	pint(stack, line_number);
-}
-void handle_pchar(stack_t **stack, char *argument, unsigned int line_number)
-{
-	(void) argument;
-	pchar_op(stack, line_number);
-}
-void handle_swap(stack_t **stack, char *argument, unsigned int line_number)
-{
-	(void) argument;
-	swap(stack, line_number);
-}
-void handle_pop(stack_t **stack, char *argument, unsigned int line_number)
-{
-	(void) argument;
-	pop(stack, line_number);
-}
-void handle_add(stack_t **stack, char *argument, unsigned int line_number)
-{
-	(void) argument;
-	add(stack, line_number);
-}
-void handle_sub(stack_t **stack, char *argument, unsigned int line_number)
-{
-	(void) argument;
-	sub(stack, line_number);
-}
-void handle_mul(stack_t **stack, char *argument, unsigned int line_number)
-{
-	(void) argument;
-	mul_op(stack, line_number);
-}
-void handle_div(stack_t **stack, char *argument, unsigned int line_number)
-{
-	(void) argument;
-	div_op(stack, line_number);
-}
-void handle_mod(stack_t **stack, char *argument, unsigned int line_number)
-{
-	(void) argument;
-	mod_op(stack, line_number);
-}
-/**
- * process_opcode - Processes the given opcode
- * @stack: Double pointer to the top of the stack
- * @opcode: The opcode to process
- * @argument: The argument passed with the opcode
- * @line_number: Current line number
- */
-void process_opcode(stack_t **stack, char *opcode, char *argument, unsigned int line_number)
-{
-	int i = 0;
-	
-	instruction_t ops[] = {
-		{"push", handle_push},
-		{"pall", handle_pall},
-		{"pint", handle_pint},
-		{"pchar", handle_pchar},
-		{"swap", handle_swap},
-		{"pop", handle_pop},
-		{"add", handle_add},
-		{"sub", handle_sub},
-		{"mul", handle_mul},
-		{"div", handle_div},
-		{"mod", handle_mod},
-		{NULL, NULL}
-	};
-	while (ops[i].opcode)
-	{
-		if (strcmp(opcode, ops[i].opcode) == 0)
-		{
-			ops[i].f(stack, argument, line_number);
-			return;
-		}
-		i++;
-	}
-	fprintf(stderr, 
-		"L%d: unknown instruction %s\n", 
-		line_number, opcode);
-	free_stack(stack);
-	exit(EXIT_FAILURE);
 }
 
+/**
+ * main - Entry point for Monty interpreter
+ * @argc: Argument count
+ * @argv: Array of argument strings
+ *
+ * Return: EXIT_SUCCESS on success, or EXIT_FAILURE on failure
+ */
+int main(int argc, char **argv)
+{
+	FILE *file;
+	char *line = NULL, *opcode, *argument;
+	char *temp_line;
+	size_t len = 0;
+	unsigned int line_number = 0;
+	stack_t *stack = NULL;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (getline(&line, &len, file) != -1)
+	{
+		line_number++;
+		temp_line = line;
+		while (*temp_line && isspace(*temp_line))
+			temp_line++;
+		/* If line is a comment or empty, skip processing */
+		if (*temp_line == '#' || *temp_line == '\0')
+			continue;
+		opcode = strtok(temp_line, " \n\t");
+		argument = strtok(NULL, " \n\t");
+		process_opcode(&stack, opcode, argument, line_number);
+		temp_line = line;
+	}
+	free(line);
+	free_stack(&stack);
+	fclose(file);
+	return (EXIT_SUCCESS);
+}
